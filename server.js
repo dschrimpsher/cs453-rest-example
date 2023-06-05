867f45f7f3e22a99e60aaa7f3941b28fc5ae3307
dschrimpsher <dan.schrimpsher@gmail.com>
2023-06-05 02:59:40 -0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const { MongoClient } = require('mongodb');
+
+// Create an Express application
+const app = express();
+let db;
+// MongoDB connection URI
+const uri = 'mongodb://mongo:27017/mydatabase';
+
+async function dbConnect() {
+// Connect to MongoDB
+  const client = await MongoClient.connect(uri);
+  console.log('Connected to MongoDB');
+  db = await client.db('mydatabase');
+  const result = await db.collection('customers').insertOne(
+    {
+      name: 'John Doe',
+      age: 30,
+      city: 'New York',
+      country: 'USA',
+    }
+  );
+  console.log(result);
+}
+
+// Define a route handler for the root path
+app.get('/customers', (req, res) => {
+    db.collection('customers').find().toArray().then((docs) => {
+      res.json(docs);
+    }).catch(err => {
+      console.error('Failed to fetch documents from MongoDB:', err);
+      res.status(500).send('Internal Server Error');
+    })
+});
+
+app.post('/customers', (req, res) => {
+  const body = req.body;
+  db.collection('customers').insertOne(body).then((docs) => {
+    res.status(201).send({success: true});
+  }).catch(err => {
+    console.error('Failed to insert customer to MongoDB:', err);
+    res.status(500).send('Internal Server Error');
+  })
+});
+
+// Start the server on port 3000
+app.listen(3000, () => {
+  dbConnect().then (() => {
+    console.log('Server is running on port 3000');
+  });
+});
+
